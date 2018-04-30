93a48a6d8f3a880695cd15891eaf0d596345cec2
zacharyburrows <zburrows7@gmail.com>
2018-04-29 22:50:34 -0700

    functional, basic tests implemented

--- blob_finder.py
+++ blob_finder.py
@@ -1,16 +1,3 @@
-"""
-TODO
-- official comment format
-- make your code beautiful and easy to read
-"""
-
-"""
-Guarantees:
-- Input file will only contain: "X", " ", "O", and newlines
-- Length of each row will be equal
-- Length of each column will be equal
-- Size will be less than 250x250
-"""
 from sys import argv
 
 
@@ -21,80 +8,77 @@ def main(argv):
     test_file = open(argv[1], "r")
 
     #move into 2D list of characters
-    array = parse_blob_file(test_file)
+    blob_2D = parse_blob_file(test_file)
     test_file.close()
     
     #find max blob sizes for blob_elements and store in result
     result = {}
     for element in blob_elements:
-        result[element] = find_max_blob(array, element)
+        result[element] = find_max_blob(blob_2D, element)
 
     print(result)
 
-        
+
 def parse_blob_file(test_file):
-    array = []
+    blob_2D = []
     for line in test_file:
         #strip guaranteed newline
         line = line[:len(line)-1]
         #split by spaces
-        array.append(line.split(" "))
+        blob_2D.append(line.split(" "))
 
     #convert to 2d array of dicts whose value contains a "visited" element
-    for row in range(len(array)):
-        for col in range(len(array[0])):
-            array[row][col] = { array[row][col] : 0 }
+    for row in range(len(blob_2D)):
+        for col in range(len(blob_2D[0])):
+            blob_2D[row][col] = { blob_2D[row][col] : 0 }
     
-    return array
+    return blob_2D
 
 
 """
+FUNCTION:
 Finds maximum blob size of "val" within an array
 Returns max blob size as an int
 
 OPTIMIZATION:
-I think this could be more time efficient if text values in
-the array were  replaced with objects whose values included
-a "visited" element
+I've replaced text values in the array with dicts whose keys
+are the text, and value is a "visited" element
 """
-def find_max_blob(array, val):
+def find_max_blob(blob_2D, val):
     max_blob_size = 0
-    for row in range(len(array)):
-        for col in range(len(array[0])):
-            if val in array[row][col] and array[row][col][val] == 0:
-                cur_blob_size = blob_counter(array, val, row, col, 1)
+    for row in range(len(blob_2D)):
+        for col in range(len(blob_2D[0])):
+            if val in blob_2D[row][col] and blob_2D[row][col][val] == 0:
+                cur_blob_size = blob_counter(blob_2D, val, row, col, 0)
                 if cur_blob_size > max_blob_size:
                     max_blob_size = cur_blob_size
     return max_blob_size
 
 
-def blob_counter(array, val, row, col, blob_size):
-    #visit current
-    array[row][col][val] = 1
+def blob_counter(blob_2D, val, row, col, blob_size):
+    #"visit" current element and increment blob_size
+    blob_2D[row][col][val] = 1
+    blob_size += 1
     
     #check right (border? matching value? visited?)
-    if col < len(array[0])-1:
-        if (val in array[row][col+1]) and (array[row][col+1][val] == 0):
-            blob_size += 1
-            blob_counter(array, val, row, col+1, blob_size)
+    if col < len(blob_2D[0])-1:
+        if (val in blob_2D[row][col+1]) and (blob_2D[row][col+1][val] == 0):
+            blob_size = blob_counter(blob_2D, val, row, col+1, blob_size)
             
     #check down
-    if row < len(array)-1:
-        if (val in array[row+1][col]) and (array[row+1][col][val] == 0):
-            blob_size += 1
-            blob_counter(array, val, row+1, col, blob_size)
+    if row < len(blob_2D)-1:
+        if (val in blob_2D[row+1][col]) and (blob_2D[row+1][col][val] == 0):
+            blob_size = blob_counter(blob_2D, val, row+1, col, blob_size)
             
     #check left
     if col > 0:
-        if (val in array[row][col-1]) and (array[row][col-1][val] == 0):
-            blob_size += 1
-            blob_counter(array, val, row, col-1, blob_size)
+        if (val in blob_2D[row][col-1]) and (blob_2D[row][col-1][val] == 0):
+            blob_size = blob_counter(blob_2D, val, row, col-1, blob_size)
             
     #check up
     if row > 0:
-        if (val in array[row-1][col]) and (array[row-1][col][val] == 0):
-            blob_size += 1
-            blob_counter(array, val, row-1, col, blob_size)
+        if (val in blob_2D[row-1][col]) and (blob_2D[row-1][col][val] == 0):
+            blob_size = blob_counter(blob_2D, val, row-1, col, blob_size)
 
     return blob_size
 
--- test_blob_finder.py
+++ test_blob_finder.py
@@ -1,33 +1,101 @@
-#test file
 import unittest
 import blob_finder
+import os
+import tempfile
+
+"""
+TEST#2
+
+"""
 
 class TestBlobFinder(unittest.TestCase):
-    
-    def test_find_max_blob(self):
-        test_array = [['X','X','X'],
-                      ['X','X','X'],
-                      ['X','X','X']]
-        
-        self.assertEqual(main.find_max_blob(test_array), 9)
+    """
+    TEST#1
+    a) Write sample data to a temporary file
+    b) Parse with the parse_blob_file function
+    c) Call find_blobs on the data and test the result for expected output.
+    """
+    def test_blob_counter(self):
+        with tempfile.TemporaryFile('w+') as tmp:
+            tmp.write("X X X X\n"
+                      "X O O X\n"
+                      "X O O X\n"
+                      "X X X X\n")
+                
+            tmp.seek(0)
+            blob_2D = blob_finder.parse_blob_file(tmp)
+            blob_size = blob_finder.blob_counter(blob_2D, "O", 1, 1, 0)
+            self.assertEqual(blob_size, 4)
 
+            tmp.seek(0)
+            blob_size = blob_finder.blob_counter(blob_2D, "X", 0, 0, 0)
+            self.assertEqual(blob_size, 12)
+
+        with tempfile.TemporaryFile('w+') as tmp:
+            tmp.write("X X O O\n"
+                      "O X O O\n"
+                      "O X X O\n"
+                      "O O X X\n")
+                
+            tmp.seek(0)
+            blob_2D = blob_finder.parse_blob_file(tmp)
+            blob_size = blob_finder.blob_counter(blob_2D, "X", 3, 3, 0)
+            self.assertEqual(blob_size, 7)
+
+            tmp.seek(0)
+            blob_size = blob_finder.blob_counter(blob_2D, "O", 3, 0, 0)
+            self.assertEqual(blob_size, 4)
+
+    def test_find_max_blob(self):
+        with tempfile.TemporaryFile('w+') as tmp:
+            tmp.write("X X X X\n"
+                      "X O O X\n"
+                      "X O O X\n"
+                      "X X X X\n")
+                
+            tmp.seek(0)
+            blob_2D = blob_finder.parse_blob_file(tmp)
+            max_blob_size = blob_finder.find_max_blob(blob_2D, "O")
+            self.assertEqual(max_blob_size, 4)
+
+            tmp.seek(0)
+            blob_2D = blob_finder.parse_blob_file(tmp)
+            max_blob_size = blob_finder.find_max_blob(blob_2D, "X")
+            self.assertEqual(max_blob_size, 12)
+
+            with tempfile.TemporaryFile('w+') as tmp:
+                tmp.write("X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X X\n"
+                          "X X X X X X X X X O\n")
+                
+                tmp.seek(0)
+                blob_2D = blob_finder.parse_blob_file(tmp)
+                max_blob_size = blob_finder.find_max_blob(blob_2D, "O")
+                self.assertEqual(max_blob_size, 1)
+
+                tmp.seek(0)
+                blob_2D = blob_finder.parse_blob_file(tmp)
+                max_blob_size = blob_finder.find_max_blob(blob_2D, "X")
+                self.assertEqual(max_blob_size, 199)
+    
+    
 
 if __name__ == '__main__':
-    unittest.main()
-
-
-
-
-
-
-
-
-
-
-"""
-    Test Cases:
-     1) File doesn't exist
-     2) File is invalid format
-         ~ contains values other than X or O), has wrong number or rows/columns
-     3) 
-"""
+    unittest.main() 
